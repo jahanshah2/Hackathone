71ba2726d2784c721dbedc49a24db45918546999
jahanshah2 <janalishah@gmail.com>
2024-09-14 14:04:38 +0500

    milestone

---
+++ script.ts
@@ -0,0 +1,15 @@
+const toggleButton = document.getElementById(
+    "toggle-skills"
+  ) as HTMLButtonElement;
+  const skills = document.getElementById("skills") as HTMLElement;
+  
+  // Set an initial display style
+  skills.style.display = 'block';
+  
+  toggleButton.addEventListener('click', () => {
+    if (skills.style.display === 'none' || skills.style.display === '') {
+      skills.style.display = 'block';
+    } else {
+      skills.style.display = 'none';
+    }
+  });
